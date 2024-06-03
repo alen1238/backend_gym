0fff64698fac57f3f6eba738ba819cc32943fcc0
Alfonso Lara <orozcoenrique159@gmail.com>
2024-06-03 13:30:36 -0500

    commit inicial del repositorio

---
+++ pruebasLogicaPropo/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("p\tq\tp AND q");
+Console.WriteLine("------------------");
+for (int i = 0; i <= 1; i++)
+{
+    for (int j = 0; j <= 1; j++)
+    {
+        bool p = Convert.ToBoolean(i);
+        bool q = Convert.ToBoolean(j);
+        bool resultado = p && q;
+        Console.WriteLine($"{p}\t{q}\t{resultado}");
+    }
+}
